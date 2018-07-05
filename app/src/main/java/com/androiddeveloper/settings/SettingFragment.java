4076d185503139c60b134014e2a51f671dcea575
AzharAsghar <Azharaenaa001500@gmail.com>
2018-07-05 16:49:41 +0500

    Create all preferences with onclick events

--- app/src/main/java/com/androiddeveloper/settings/SettingFragment.java
+++ app/src/main/java/com/androiddeveloper/settings/SettingFragment.java
@@ -11,6 +11,7 @@ import android.support.v7.preference.PreferenceManager;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.SwitchPreferenceCompat;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -19,98 +20,167 @@ import java.util.Locale;
  */
 
 public class SettingFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
-    public SwitchPreferenceCompat auto_scan, clear_cache;
-    Preference dialogPrefernce, languagePeference,quitPreferences;
-    private SharedPreferences preferences;
+      private SharedPreferences preferences;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.prefere);
 
 
-//        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
-//        PreferenceScreen prefScre = getPreferenceScreen();
-//        int count = prefScre.getPreferenceCount();
-//        for(int i = 0; i<count;i++){
-//            Preference preference = prefScre.getPreference(i);
-//            if(!(preference instanceof CheckBoxPreference)){
-//                String value = sharedPreferences.getString(preference.getKey(),"");
-//                setPreferenceSummary(preference,value);
-//            }
-//        }
-        languagePeference = (Preference) findPreference(getResources().getString(R.string.language_key));
-        quitPreferences = (Preference) findPreference(getResources().getString(R.string.quit_key));
-        dialogPrefernce = (Preference) findPreference(getResources().getString(R.string.reset_key));
-        auto_scan = (SwitchPreferenceCompat) findPreference(getResources().getString(R.string.auto_scan_key)); //Preference Key
-        clear_cache = (SwitchPreferenceCompat) findPreference(getResources().getString(R.string.clear_cache_key));
-//        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
-//        PreferenceScreen prefScre = getPreferenceScreen();
-//        int count = prefScre.getPreferenceCount();
-//        for(int i = 0; i<count;i++){
-//            Preference preference = prefScre.getPreference(i);
-//            if(!(preference instanceof CheckBoxPreference)){
-//                String value = sharedPreferences.getString(preference.getKey(),"");
-//                setPreferenceSummary(preference,value);
-//            }
-//        }
-        String locale = this.getResources().getConfiguration().locale.getDisplayName();
-//        String LAnglocale = locale.getDefault().getDisplayName();
-        String lan = Locale.getDefault().getDisplayLanguage();
-        String langCode = Locale.getDefault().getLanguage(); //to get usual language code
 
-        System.out.println(lan + "\n" + langCode + "\n" + locale);
-        languagePeference.setSummary(locale);
+
+
         getPreferenceManager().
                 findPreference(getResources()
-                        .getString(R.string.language_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                        .getString(R.string.salary_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
 
-                Intent intent = new Intent(getContext(),MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                return false;
-            }
-        });
-        getPreferenceManager().
-                findPreference(getResources()
-                        .getString(R.string.quit_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(getActivity(),getResources()
+                        .getString(R.string.salary_key),Toast.LENGTH_LONG).show();
 
-//                Intent intent = new Intent(getContext(),VideoFolder.class);
+//                Intent intent = new Intent(getContext(),MainActivity.class);
 //                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 //                startActivity(intent);
                 return false;
             }
         });
 
-                getPreferenceManager().findPreference(getResources().getString(R.string.reset_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
 
-            public boolean onPreferenceClick(final Preference preference) {
-                AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
-                alertDialog.setMessage("Are you sure you want to reset all settings to default?");
-                alertDialog.setCancelable(true);
-                alertDialog.setButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-                        SharedPreferences.Editor editor = preferences.edit();
-                        editor.clear();
-                        editor.apply();
-                        boolean test = preferences.getBoolean(getResources().getString(R.string.auto_scan_key), true);
-                        System.out.println("RESET"+test);
+        getPreferenceManager().
+                findPreference(getResources()
+                        .getString(R.string.job_sector_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
 
-                    }
-                });
-                alertDialog.setButton2("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                alertDialog.show();
+//                Intent intent = new Intent(getContext(),MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+
+                Toast.makeText(getActivity(),getResources()
+                        .getString(R.string.job_sector_key),Toast.LENGTH_LONG).show();
                 return false;
             }
         });
+
+        getPreferenceManager().
+                findPreference(getResources()
+                        .getString(R.string.privacy_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+//                Intent intent = new Intent(getContext(),MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+
+                Toast.makeText(getActivity(),getResources()
+                        .getString(R.string.privacy_key),Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+
+        getPreferenceManager().
+                findPreference(getResources()
+                        .getString(R.string.notification_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+//                Intent intent = new Intent(getContext(),MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+
+                Toast.makeText(getActivity(),getResources()
+                        .getString(R.string.notification_key),Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+
+
+        getPreferenceManager().
+                findPreference(getResources()
+                        .getString(R.string.terms_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+//                Intent intent = new Intent(getContext(),MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+
+                Toast.makeText(getActivity(),getResources()
+                        .getString(R.string.terms_key),Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+        getPreferenceManager().
+                findPreference(getResources()
+                        .getString(R.string.privacy_policy_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+//                Intent intent = new Intent(getContext(),MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+
+                Toast.makeText(getActivity(),getResources()
+                        .getString(R.string.privacy_policy_key),Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+        getPreferenceManager().
+                findPreference(getResources()
+                        .getString(R.string.share_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+//                Intent intent = new Intent(getContext(),MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+
+                Toast.makeText(getActivity(),getResources()
+                        .getString(R.string.share_key),Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+        getPreferenceManager().
+                findPreference(getResources()
+                        .getString(R.string.delete_account_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+//                Intent intent = new Intent(getContext(),MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+
+                Toast.makeText(getActivity(),getResources()
+                        .getString(R.string.delete_account_key),Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+
+
+        getPreferenceManager().
+                findPreference(getResources()
+                        .getString(R.string.signout_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+//                Intent intent = new Intent(getContext(),MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+
+                Toast.makeText(getActivity(),getResources()
+                        .getString(R.string.signout_key),Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+
+
     }
 
     @Override
@@ -122,41 +192,41 @@ public class SettingFragment extends PreferenceFragmentCompat implements SharedP
 //                setPreferenceSummary(pref,value);
 //            }
 //        }
-        String locale = this.getResources().getConfiguration().locale.getDisplayName();
-//        String LAnglocale = locale.getDefault().getDisplayName();
-        String lan = Locale.getDefault().getDisplayLanguage();
-        String langCode = Locale.getDefault().getLanguage(); //to get usual language code
-
-        System.out.println(lan + "\n" + langCode + "\n" + locale);
-        if (key.equals(getResources().getString(R.string.language_key))) {
-            languagePeference.setSummary(locale);
-        }
-        if (key.equals(getResources().getString(R.string.auto_scan_key))) {
-            boolean test = sharedPreferences.getBoolean(getResources().getString(R.string.auto_scan_key), true);
-            //Do whatever you want here. This is an example.
-            if (test) {
-//                testPref.setSummary("Enabled");
-                System.out.println(test);
-            } else {
-//                testPref.setSummary("Disabled");
-                System.out.println(test);
-            }
-
-            if (key.equals(getResources().getString(R.string.clear_cache_key))) {
-                boolean clear_cache = sharedPreferences.getBoolean(getResources().getString(R.string.clear_cache_key), true);
-                //Do whatever you want here. This is an example.
-                if (clear_cache) {
-//                testPref.setSummary("Enabled");
-                    System.out.println(clear_cache);
-                } else {
-//                testPref.setSummary("Disabled");
-                    System.out.println(clear_cache);
-                }
-
-
-            }
-
-        }
+//        String locale = this.getResources().getConfiguration().locale.getDisplayName();
+////        String LAnglocale = locale.getDefault().getDisplayName();
+//        String lan = Locale.getDefault().getDisplayLanguage();
+//        String langCode = Locale.getDefault().getLanguage(); //to get usual language code
+//
+//        System.out.println(lan + "\n" + langCode + "\n" + locale);
+//        if (key.equals(getResources().getString(R.string.language_key))) {
+//            languagePeference.setSummary(locale);
+//        }
+//        if (key.equals(getResources().getString(R.string.auto_scan_key))) {
+//            boolean test = sharedPreferences.getBoolean(getResources().getString(R.string.auto_scan_key), true);
+//            //Do whatever you want here. This is an example.
+//            if (test) {
+////                testPref.setSummary("Enabled");
+//                System.out.println(test);
+//            } else {
+////                testPref.setSummary("Disabled");
+//                System.out.println(test);
+//            }
+//
+//            if (key.equals(getResources().getString(R.string.clear_cache_key))) {
+//                boolean clear_cache = sharedPreferences.getBoolean(getResources().getString(R.string.clear_cache_key), true);
+//                //Do whatever you want here. This is an example.
+//                if (clear_cache) {
+////                testPref.setSummary("Enabled");
+//                    System.out.println(clear_cache);
+//                } else {
+////                testPref.setSummary("Disabled");
+//                    System.out.println(clear_cache);
+//                }
+//
+//
+//            }
+//
+//        }
     }
 
 //    private void setPreferenceSummary(Preference preference, String value) {
@@ -174,22 +244,22 @@ public class SettingFragment extends PreferenceFragmentCompat implements SharedP
     public void onResume() {
         super.onResume();
 
-        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-        boolean clear_cache = preferences.getBoolean(getResources().getString(R.string.clear_cache_key), true);
-        if (clear_cache) {
-//                testPref.setSummary("Enabled");
-            System.out.println(clear_cache);
-        } else {
-//                testPref.setSummary("Disabled");
-            System.out.println(clear_cache);
-        }
-        boolean test = preferences.getBoolean(getResources().getString(R.string.auto_scan_key), true);
-
-        if (test) {
-//            testPref.setSummary("Enabled");
-        } else {
-//            testPref.setSummary("Disabled");
-        }
+//        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+//        boolean clear_cache = preferences.getBoolean(getResources().getString(R.string.clear_cache_key), true);
+//        if (clear_cache) {
+////                testPref.setSummary("Enabled");
+//            System.out.println(clear_cache);
+//        } else {
+////                testPref.setSummary("Disabled");
+//            System.out.println(clear_cache);
+//        }
+//        boolean test = preferences.getBoolean(getResources().getString(R.string.auto_scan_key), true);
+//
+//        if (test) {
+////            testPref.setSummary("Enabled");
+//        } else {
+////            testPref.setSummary("Disabled");
+//        }
     }
 
     @Override
